7ac81a28e54b892f1332923ed6149daa8252fa78
Eben Angeline <ebenangeline6@gmail.com>
2026-07-05 18:22:45 -0500

    Working in progess

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import './App.css'
 
 function App() {
  return(
-<p>"Hello, LaunchCode"</p>
+  <>
+  <Header/>
+  <main>
+    <h2>
+      Welcome to My Personal Finance Tracker Page
+    </h2>
+  </main>
+  <Footer/>
+
+</>
    );
 }
 
---
+++ Project1/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer ()
+{
+    return(
+        <footer className="footer">
+            <p>&copy;{new Date().getFullYear} My Finance App</p>
+        </footer>
+    );
+}
+export default Footer
